2040f03ff4f1cd3aa8c969ec6901f05464e10ea8
sharathpend <spendya@ncsu.edu>
2024-09-13 12:29:23 -0400

    Clear old vivado logs and journals that pile up

---
+++ clear_vivado_logs.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+find . -name "*vivado_*.log" -type f -delete
+find . -name "*vivado_*.jou" -type f -delete
+find . -name "*vivado_*.str" -type f -delete
+find . -name "*vivado_*.debug" -type f -delete
