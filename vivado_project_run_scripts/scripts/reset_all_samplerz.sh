46cb0f3568d168067654f7723c3b12e3f78c8c7e
Sharath Pendyala <spendya@ncsu.edu>
2025-05-17 22:38:54 -0400

    Updated vivado scripts. Removed log file

--- vivado_project_run_scripts/scripts/reset_all_samplerz.sh
+++ vivado_project_run_scripts/scripts/reset_all_samplerz.sh
@@ -3,5 +3,5 @@
 PROJECT_FILE=$1
 NUM_JOBS=$2
 
-vivado -mode batch -source run_all_samplerz_vusp.tcl -tclargs "$PROJECT_FILE" "$NUM_JOBS"
+vivado -mode batch -source reset_all_samplerz.tcl -tclargs "$PROJECT_FILE" "$NUM_JOBS"
 
--- vivado_project_run_scripts/scripts/run_all_samplerz.sh
+++ vivado_project_run_scripts/scripts/run_all_samplerz.sh
@@ -3,4 +3,4 @@
 PROJECT_FILE=$1
 NUM_JOBS=$2
 
-vivado -mode batch -source run_all_samplerz_zusp.tcl -tclargs "$PROJECT_FILE" "$NUM_JOBS"
+vivado -mode batch -source run_all_samplerz.tcl -tclargs "$PROJECT_FILE" "$NUM_JOBS"
