4d34a43386f34eeb879b76d9c13c33a9f4030d10
sharathpend <150897824+sharathpend@users.noreply.github.com>
2025-05-19 12:02:44 -0400

    Update clear_vivado_logs.sh

--- clear_vivado_logs.sh
+++ clear_vivado_logs.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
-find . -name "*vivado_*.log" -type f -delete
-find . -name "*vivado_*.jou" -type f -delete
+# Additional cleanup if -a flag is specified
+if [ "$1" = "-a" ]; then
+    echo "Performing extended cleanup..."
+    find . -name "*vivado*.log" -type f -delete
+    find . -name "*vivado*.jou" -type f -delete
+else
+    echo "Performing standard cleanup..."
+    find . -name "*vivado_*.log" -type f -delete
+    find . -name "*vivado_*.jou" -type f -delete
+fi
+
+# Default cleanup
 find . -name "*vivado_*.str" -type f -delete
 find . -name "*vivado_*.debug" -type f -delete
